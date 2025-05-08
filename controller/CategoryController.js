1b281e941ffae056471a4aa8d6e2f5f48530473c
Geethan chanaka <geethan.chanaka@arimaclanka.com>
2025-05-08 12:18:49 +0530

    update category

--- controller/CategoryController.js
+++ controller/CategoryController.js
@@ -4,9 +4,9 @@ const CategorySchema = require("../model/CategorySchema");
 //Save (POST)
 const createCategory = async (request, response) => {
   try {
-    const { categoryName , availableCountries } = request.body;
+    const { categoryName, availableCountries } = request.body;
 
-    if (!categoryName ||!availableCountries ) {
+    if (!categoryName || !availableCountries) {
       return response
         .status(400)
         .json({ code: 400, message: "Some fields are missing", data: null });
@@ -41,32 +41,27 @@ const createCategory = async (request, response) => {
 //update(PUT)
 const updateCategory = async (request, response) => {
   try {
-    const { categoryName , availableCountries } = request.body;
+    const { categoryName, availableCountries } = request.body;
 
-    if (!categoryName ||!availableCountries ) {
+    if (!categoryName) {
       return response
         .status(400)
         .json({ code: 400, message: "Some fields are missing", data: null });
     }
 
-    const category = new CategorySchema({
-      categoryName: categoryName,
-      icon: {
-        hash: "Temp Hash",
-        resourceUrl:
-          "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.freepik.com%2Ffree-photos-vectors%2Fprofile&psig=AOvVaw1dL2XK2h1UXCRNO8mBCy_R&ust=1746686162496000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCJjNsJXfkI0DFQAAAAAdAAAAABAE",
-        fileName: "Temp File Name",
-        directory: "Temp Directory",
-      },
-      availableCountries: availableCountries,
-    });
+    const updateData = await CategorySchema.findOneAndUpdate(
+      { _id: request.params.id },
+      {
+        $set: {
+          categoryName: categoryName,
+        },
+      }
+    );
 
-    const saveData = await category.save();
-
-    return response.status(201).json({
-      code: 201,
-      message: "customer has been saved...",
-      data: saveData,
+    return response.status(200).json({
+      code: 200,
+      message: "customer has been updated...",
+      data: updateData,
     });
   } catch (e) {
     response
