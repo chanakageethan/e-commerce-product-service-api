10c6db12d7b7fc5e729684adf2e046d30eac3361
Geethan chanaka <geethan.chanaka@arimaclanka.com>
2025-05-08 06:07:29 +0530

    create category

--- controller/CategoryController.js
+++ controller/CategoryController.js
@@ -1,32 +1,92 @@
 const { request, response } = require("express");
 const CategorySchema = require("../model/CategorySchema");
 
-
 //Save (POST)
-const createCategory =  (request, response) => {
-  console.log("Inside create category")
-  console.log(request.body);
+const createCategory = async (request, response) => {
+  try {
+    const { categoryName , availableCountries } = request.body;
+
+    if (!categoryName ||!availableCountries ) {
+      return response
+        .status(400)
+        .json({ code: 400, message: "Some fields are missing", data: null });
+    }
+
+    const category = new CategorySchema({
+      categoryName: categoryName,
+      icon: {
+        hash: "Temp Hash",
+        resourceUrl:
+          "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.freepik.com%2Ffree-photos-vectors%2Fprofile&psig=AOvVaw1dL2XK2h1UXCRNO8mBCy_R&ust=1746686162496000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCJjNsJXfkI0DFQAAAAAdAAAAABAE",
+        fileName: "Temp File Name",
+        directory: "Temp Directory",
+      },
+      availableCountries: availableCountries,
+    });
+
+    const saveData = await category.save();
+
+    return response.status(201).json({
+      code: 201,
+      message: "customer has been saved...",
+      data: saveData,
+    });
+  } catch (e) {
+    response
+      .status(500)
+      .json({ code: 500, message: "something went wrong", data: e });
+  }
 };
 
 //update(PUT)
-const updateCategory =  (request, response) => {
-  console.log(request.body);
+const updateCategory = async (request, response) => {
+  try {
+    const { categoryName , availableCountries } = request.body;
+
+    if (!categoryName ||!availableCountries ) {
+      return response
+        .status(400)
+        .json({ code: 400, message: "Some fields are missing", data: null });
+    }
+
+    const category = new CategorySchema({
+      categoryName: categoryName,
+      icon: {
+        hash: "Temp Hash",
+        resourceUrl:
+          "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.freepik.com%2Ffree-photos-vectors%2Fprofile&psig=AOvVaw1dL2XK2h1UXCRNO8mBCy_R&ust=1746686162496000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCJjNsJXfkI0DFQAAAAAdAAAAABAE",
+        fileName: "Temp File Name",
+        directory: "Temp Directory",
+      },
+      availableCountries: availableCountries,
+    });
+
+    const saveData = await category.save();
+
+    return response.status(201).json({
+      code: 201,
+      message: "customer has been saved...",
+      data: saveData,
+    });
+  } catch (e) {
+    response
+      .status(500)
+      .json({ code: 500, message: "something went wrong", data: e });
+  }
 };
 
-
 //delete (DELETE)
-const deleteCategory =  (request, response) => {
+const deleteCategory = (request, response) => {
   console.log(request.body);
 };
 
 //find by id (GET)
-const findCategoryById =  (request, response) => {
+const findCategoryById = (request, response) => {
   console.log(request.body);
 };
 
-
 //find all (GET)
-const findAllCategories =  (request, response) => {
+const findAllCategories = (request, response) => {
   console.log(request.body);
 };
 
